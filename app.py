9aa4bddfb81922656fb5cd26df12a7932a3c2bc1
pqpq2099 <pqpq2099@gmail.com>
2026-03-04 15:26:57 +0800

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from core.ui_style import apply_global_style
-from core.session import init_session
-
-apply_global_style()
-init_session()
-
-st.title("OMS 2.0")
-st.caption("請使用左側側欄進入：Home / Inventory / Export / Analysis / History")
